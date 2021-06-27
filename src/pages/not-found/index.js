c14d439fe245b6eb44716feed2b573de322171d6
TanTano796 <tantn1@sunshinegroup.vn>
2021-06-27 15:57:28 +0700

    not found page

---
+++ src/pages/not-found/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+
+function NotFoundPage() {
+    return (
+        <img className="not-found" src="/assets/images/common/not-found-page.jpg"/>
+    );
+}
+
+export default NotFoundPage;
