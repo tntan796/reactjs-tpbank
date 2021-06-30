1eb15b200d9a78900bdd658a9d0a8c7f37ae6eef
Tan Tano <tantn1@sunshinegroup.vn>
2021-06-30 09:31:12 +0700

    setting upgrade

--- src/pages/setting/upgrade/index.js
+++ src/pages/setting/upgrade/index.js
@@ -1,6 +1,24 @@
+import './index.css';
 function SettingUpgragePage() {
     return (
-        <div>SettingUpgragePage</div>
+        <div className="content-wrap">
+            <div className="introduce">
+                <div className="introduce__wrap">
+                    <div>
+                        <p> TPBank eBank quy định các hạn mức giao dịch khác nhau tương ứng
+                            với từng loại tài khoản. Loại tài khoản của Quý khách được xác định dựa trên trạng thái
+                            hoàn thiện thông tin với ngân hàng. Đối với tài khoản đã hoàn thiện thông tin (cấp tài
+                            khoản cao nhất), Quý khách sẽ được giao dịch với hạn mức tối đa tương ứng với Phương
+                            thức bảo mật được sử dụng và Loại giao dịch đang thực hiện. </p>
+                        <p> Khi thực hiện nâng cấp tài khoản, Quý khách sẽ được tự động nâng
+                            hạn mức tương ứng, đồng thời được truy cập và sử dụng thêm các tính năng nâng cao của
+                            dịch vụ eBank. </p>
+                        <p> Để nâng cấp tài khoản, Quý khách vui lòng mang theo CMND/CCCD/Hộ chiếu tới các
+                            <a href="https://tpb.vn/lien-he/tim-diem-giao-dich">điểm giao dịch gần nhất của TPBank</a> để hoàn thiện thủ tục. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
