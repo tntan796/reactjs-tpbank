4841791da47174e2cc1260f53cb2f1ab0f0dfae1
Tan Tano <tantn1@sunshinegroup.vn>
2021-06-30 10:16:21 +0700

    setting support

--- src/pages/setting/support/index.js
+++ src/pages/setting/support/index.js
@@ -1,6 +1,18 @@
+import './index.css';
 function SettingSupportPage() {
     return (
-        <div>SettingSupportPage</div>
+        <div className="content-wrap">
+            <div className="support">
+                <div className="support__wrap-item first-item">
+                    <span className="support__email-title">Email</span>
+                    <span className="support__email">dichvu_khachhang@abc.com.vn</span>
+                </div>
+                <div className="support__wrap-item">
+                    <span className="support__call-center">Hotline: 1900 585 885</span>
+                    <span className="support__hotline">Tổng đài hỗ trợ</span>
+                </div>
+            </div>
+        </div>
     );
 }
 
