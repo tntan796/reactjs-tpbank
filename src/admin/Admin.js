5c3aaa8175871b1918ac0749feff75b76e8cbd87
TanTano796 <tantn1@sunshinegroup.vn>
2021-07-11 22:54:48 +0700

    add layout admin

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,8 +1,69 @@
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import './Admin.css';
 
+import { Button } from 'primereact/button';
 function Admin() {
     return (
-        <div>Admin</div>
+        <div className="layout-wrapper"> 
+         {/* layout-topbar-mobile-active */}
+            <div className="layout-topbar p-shadow-4">
+                <div className="layout-topbar-left">
+                    <button type="button" className="layout-topbar-logo p-link" style={{ cursor: 'pointer' }}>
+                        <img id="app-logo" src="https://www.primefaces.org/ultima-react/assets/layout/images/logo-light.svg" alt="ultima-layout" style={{ height: '2.25rem' }} />
+                    </button>
+                    <button type="button" className="layout-menu-button p-shadow-6 p-link">
+                        <i className="pi pi-chevron-right" />
+                    </button>
+                    <button type="button" className="layout-topbar-mobile-button p-link">
+                        <i className="pi pi-ellipsis-v fs-large" />
+                    </button>
+                </div>
+                <div className="layout-topbar-right">
+                {/* layout-topbar-mobile-active */}
+
+                    <div className="layout-topbar-actions-left">
+                    </div>
+                    <div className="layout-topbar-actions-right">
+                        <ul className="layout-topbar-items">
+                            <li className="layout-topbar-item layout-search-item">
+                                <button className="layout-topbar-action rounded-circle p-link">
+                                    <i className="pi pi-search fs-large" />
+                                </button>
+                            </li>
+                            <li className="layout-topbar-item notifications">
+                                <button className="layout-topbar-action rounded-circle p-link">
+                                    <span className="p-overlay-badge">
+                                        <i className="pi pi-bell fs-large" />
+                                        <span className="p-badge p-badge-warning p-badge-dot" />
+                                    </span>
+                                </button>
+                            </li>
+                            <li className="layout-topbar-item app">
+                                <button className="layout-topbar-action rounded-circle p-link">
+                                    <i className="pi pi-table fs-large" />
+                                </button>
+                            </li>
+                            <li className="layout-topbar-item">
+                                <button className="layout-topbar-action p-d-flex p-dir-row p-jc-center p-ai-center p-px-2 rounded-circle p-link">
+                                    <img src="https://www.primefaces.org/ultima-react/assets/demo/images/avatar/amyelsner.png" alt="avatar" style={{ width: '32px', height: '32px' }} />
+                                </button>
+                            </li>
+                            <li className="layout-topbar-item">
+                                <button type="button" className="layout-topbar-action rounded-circle p-link">
+                                    <i className="pi fs-large pi-arrow-left" />
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="menu-wrapper">
+                
+            </div>
+            <div className="layout-main"></div>
+        </div>
     );
 }
 
